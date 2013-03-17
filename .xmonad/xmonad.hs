b05755435bd1d972ff98e588231db24658139af4
Wenchuan Weng <vvenchuan@gmail.com>
2013-03-17 00:01:36 -0700

    Xmonad config: use xscreensaver

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,13 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad defaultConfig
+main = do
+    xmonad $ defaultConfig
         { modMask = mod4Mask -- Use Super instead of Alt
-        }
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+        ]
