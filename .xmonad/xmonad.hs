8e9aa8922d1233c93adbe6f596ce96d5cc90345f
Wenchuan Weng <vvenchuan@gmail.com>
2013-03-17 01:37:55 -0700

    Xmonad config: got my self a bar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,8 +6,15 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/local/ANT/wenchuan/.xmobarrc"
     xmonad $ defaultConfig
-        { modMask = mod4Mask -- Use Super instead of Alt
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask -- Use Super instead of Alt
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
